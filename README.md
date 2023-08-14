c1a40fffb9b01cd7c169d162a90578c2757bc7a2
sujianqingfeng <121116111@qq.com>
2023-08-14 17:38:30 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# code-design-upload
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import manifest from './manifest.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    crx({manifest})
+  ],
+})
